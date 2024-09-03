37a47b5a8d1d3bec6b10b859b3b2013d5a1efb20
pavani <pavanijalagam6@gmail.com>
2024-09-03 08:57:57 +0530

    commits today 3sep

--- src/controlstatements.java
+++ src/controlstatements.java
@@ -1,2 +1,26 @@
 public class controlstatements {
+    public static void main(String[] args) {
+
+        // 100 - 5
+        // 91 to 99 - A
+        // 81 to 90 - B
+
+        // anything below that is failed
+        // if then statement
+
+        int studentScore = 101;
+
+        if (studentScore >= 100) {
+            System.out.println("You scored S grade");
+        }
+        else if (studentScore > 90 && studentScore < 100) {
+            System.out.println("You scored A grade");
+        }
+        else if (studentScore > 80 && studentScore < 91) {
+            System.out.println("You scored B grade");
+        }
+        else {
+            System.out.println("You failed");
+        }
+    }
 }
